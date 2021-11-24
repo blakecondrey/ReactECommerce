f858b5d35e977c6057fb0123a3558abee4982e23
Blake Condrey <condrey.blake1217@gmail.com>
2021-11-23 20:02:44 -0700

    sign in component designed and custom button component designed; implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 const HatsPage = () => (
   <div>
@@ -18,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
