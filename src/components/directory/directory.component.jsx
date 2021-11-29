97a53a0680d7e6e0bf5aacae2c807b70013ff5cd
Blake Condrey <condrey.blake1217@gmail.com>
2021-11-28 20:17:20 -0700

    directory images updated

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,33 +11,33 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                 {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    title: 'equipment',
+                    imageUrl: 'https://i.ibb.co/5hqTJYZ/pexels-ivan-samkov-4162446.jpg',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'shop/gear'
                 },
                 {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    title: 'fit tech',
+                    imageUrl: 'https://i.ibb.co/6B7B1ny/pexels-karolina-grabowska-4498479.jpg',
                     id: 2,
-                    linkUrl: 'shop/jackets'
+                    linkUrl: 'shop/fittech'
                 },
                 {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    title: 'footwear',
+                    imageUrl: 'https://i.ibb.co/8mLwxST/pexels-pixabay-415261.jpg',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
+                    linkUrl: 'shop/footwear'
                 },
                 {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'https://i.ibb.co/sHP8yy4/pexels-nathan-cowley-1089144.jpg',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'https://i.ibb.co/VSZb8D5/pexels-ketut-subiyanto-5039659.jpg',
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
