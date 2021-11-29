6f519883dcd9fbc77e0a9f5167a3d1d7daab3bd2
Blake Condrey <condrey.blake1217@gmail.com>
2021-11-29 07:13:34 -0700

    shopping cart item component and style implemented - no functionality yet implmtd

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-item.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
                 SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
     </div>
 );
