b1239d6504768497c74b3f6bbefdb66b152eb746
Blake Condrey <condrey.blake1217@gmail.com>
2021-12-20 08:18:20 -0700

    sep CRA into client folder and implemented express server to handle stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
 import Logo from '../../assets/crown.svg';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51K3ZCjJ8Ezj5dotcUgjelPX7r4f37mZCkFg0jW5mRW5D38w7ifMLaoc8bpU23A34dIElfacCmO7Gbh856fFpWZSS0032oOED2d';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful!');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        })
+            .then(response => {
+                alert('Payment Successful!');
+        })
+            .catch(error => {
+                console.log('Payment error: ', error);
+                alert(
+                    'There was an issue with your payment. Use provided TEST Credit Card'
+                );
+        });
     };
 
     return (
