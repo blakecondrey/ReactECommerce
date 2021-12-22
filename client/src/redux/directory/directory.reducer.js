8d72d091105cc093525d2a86db66cf06bf2a2d04
Blake Condrey <condrey.blake1217@gmail.com>
2021-12-21 18:43:32 -0700

    image files compressed

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
     sections: [
         {
             title: 'gear',
-            imageUrl: 'https://i.ibb.co/LrXdpz7/pexels-ivan-samkov-4162446.webp', 
+            imageUrl: 'https://i.ibb.co/0BBCd9J/oly-bar.png', 
             id: 1,
             linkUrl: 'shop/gear'
         },
         {
             title: 'fit tech',
-            imageUrl: 'https://i.ibb.co/QrZmSR5/pexels-karolina-grabowska-4498479.webp',
+            imageUrl: 'https://i.ibb.co/ZH8Ckqf/android.png',
             id: 2,
             linkUrl: 'shop/fittech'
         },
         {
             title: 'footwear',
-            imageUrl: 'https://i.ibb.co/MgrTfXL/pexels-pixabay-415261.webp',
+            imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
             id: 3,
             linkUrl: 'shop/footwear'
         },
         {
             title: 'womens',
-            imageUrl: 'https://i.ibb.co/bdstTnG/pexels-nathan-cowley-1089144-1.webp',
+            imageUrl: 'https://i.ibb.co/fHV4rPr/pexels-photo-1089144-1.png',
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
         },
         {
             title: 'mens',
-            imageUrl: 'https://i.ibb.co/4YTGswq/pexels-ketut-subiyanto-5039659.webp',
+            imageUrl: 'https://i.ibb.co/4ZjHDZR/pexels-photo-5039659-1-1-1.png',
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
